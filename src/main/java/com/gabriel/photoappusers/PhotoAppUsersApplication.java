3b7d3d00da524d74bdab463de08f9f83fadc36aa
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2021-05-01 13:08:32 +0200

    enable HTTP Requests login in Feign Client

--- src/main/java/com/gabriel/photoappusers/PhotoAppUsersApplication.java
+++ src/main/java/com/gabriel/photoappusers/PhotoAppUsersApplication.java
@@ -1,5 +1,6 @@
 package com.gabriel.photoappusers;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -30,9 +31,9 @@ public class PhotoAppUsersApplication {
         return new RestTemplate();
     }
 
-
-
-
-
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
 
 }
